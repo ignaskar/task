82af3cae233ebe8fc4ba3f358d9eb358c34e40b4
Ignas Karpusenkovas <ignas.karpusenkovas@gmail.com>
2023-11-25 23:03:52 +0200

    add base app configuration

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,15 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use backend::configuration;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let configuration = configuration::get_configuration().expect("main.rs - unable to read configuration file.");
+
     HttpServer::new(|| {
         App::new()
             .service(hello)
     })
-        .bind(("127.0.0.1", 8080))?
+        .bind((configuration.application.host, configuration.application.port))?
         .run()
         .await
 }
