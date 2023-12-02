7bccebbeaed71563fa27149818f883b638198839
Ignas Karpusenkovas <ignas.karpusenkovas@gmail.com>
2023-12-02 17:45:24 +0200

    less verbose error handling for getUsers

--- backend/src/api/routes/get_users.rs
+++ backend/src/api/routes/get_users.rs
@@ -1,38 +1,23 @@
-use actix_web::{HttpResponse, Responder, ResponseError, web};
-use actix_web::body::BoxBody;
-use actix_web::http::StatusCode;
-use thiserror::Error;
+use actix_web::{HttpResponse, Responder, web};
 use crate::api::contracts;
 use crate::api::contracts::GetUsersResponse;
 use crate::service::Service;
 
-pub async fn get_users(service: web::Data<Service>) -> Result<impl Responder, GetUsersError> {
-    let users = service.get_users()?;
+pub async fn get_users(service: web::Data<Service>) -> impl Responder {
+    let get_users_result = service.get_users();
+
+    let users = match get_users_result {
+        Ok(users) => users,
+        Err(_) => {
+            return HttpResponse::InternalServerError().json(contracts::Response::<()>::err(vec![contracts::Error{
+                message: "Internal server error".to_string()
+            }]))
+        }
+    };
+
     let response = contracts::Response::ok(GetUsersResponse {
         users: users.iter().map(|u| u.into()).collect()
     });
 
-    Ok(HttpResponse::Ok().json(response))
-}
-
-#[derive(Error, Debug)]
-pub enum GetUsersError {
-    #[error(transparent)]
-    Internal(#[from] anyhow::Error)
-}
-
-impl ResponseError for GetUsersError {
-    fn status_code(&self) -> StatusCode {
-        match self {
-            GetUsersError::Internal(_) => StatusCode::INTERNAL_SERVER_ERROR
-        }
-    }
-
-    fn error_response(&self) -> HttpResponse<BoxBody> {
-        HttpResponse::build(self.status_code()).json(contracts::Response::<()>::err(vec![
-            contracts::Error {
-                message: "Internal server error".to_string()
-            }
-        ]))
-    }
+    HttpResponse::Ok().json(response)
 }
