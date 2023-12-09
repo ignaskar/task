6cff5cae9c32cb9297fe151060289c6d8d10087f
Ignas Karpusenkovas <ignas.karpusenkovas@gmail.com>
2023-12-09 17:12:20 +0200

    update script

--- backend/scripts/init_db.sh
+++ backend/scripts/init_db.sh
@@ -7,30 +7,39 @@ if ! [ -x "$(command -v psql)" ]; then
   exit 1
 fi
 
+if ! [ -x "$(command -v diesel)" ]; then
+  echo >&2 "Error: diesel CLI is not installed."
+  echo >&2 "Install it with:"
+  echo >&2 "cargo install diesel_cli --no-default-features --features postgres"
+  exit 1
+fi
+
+if ! [ -x "$(command -v docker)" ]; then
+  echo >&2 "Error: docker is not installed."
+  exit 1
+fi
+
 DB_USER="${POSTGRES_USER:=postgres}"
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=users}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
-if [[ -z "${SKIP_DOCKER}" ]]
-then
-  RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres' --format '{{.ID}}')
-  if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
-    echo >&2 "there is a postgres container already running, kill it with"
-    echo >&2 "    docker kill ${RUNNING_POSTGRES_CONTAINER}"
-    exit 1
-  fi
-
-  docker run \
-      -e POSTGRES_USER=${DB_USER} \
-      -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-      -e POSTGRES_DB=${DB_NAME} \
-      -p "${DB_PORT}":5432 \
-      -d \
-      --name "postgres_$(date '+%s')" \
-      postgres -N 75
+RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres' --format '{{.ID}}')
+if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
+  echo >&2 "there is a postgres container already running, kill it with"
+  echo >&2 "    docker kill ${RUNNING_POSTGRES_CONTAINER}"
+  exit 1
 fi
+
+docker run \
+    -e POSTGRES_USER=${DB_USER} \
+    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+    -e POSTGRES_DB=${DB_NAME} \
+    -p "${DB_PORT}":5432 \
+    -d \
+    --name "postgres_$(date '+%s')" \
+    postgres -N 75
 
 until PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
   >&2 echo "Postgres is still unavailable - sleeping"
